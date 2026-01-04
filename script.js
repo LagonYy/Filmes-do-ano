16e2ccdd88328f25e1422a1b8a8f4b08dd3decb8
Lara Rodrigues <larabeatrizar@outlook.com>
2026-01-04 17:43:36 -0300

    mudando img

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const filmes = {
         },
         {
             title: "Matrix",
-            poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/qFHE09iQDbpHUBXJ8cFZu9v2ViO.jpg"
+            poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/dqFTOD2Al6ovvDi046mQqnLBLwc.jpg"
         },
     ],
 
