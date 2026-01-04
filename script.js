ab1ae55380aa148a99dd0f4a03664a8e7e41b7f9
Lara Rodrigues <larabeatrizar@outlook.com>
2026-01-04 17:32:31 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const filmes = {
+    jan: [
+        {
+            title: "Prenda-me se for Capaz",
+            poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/pSaqcj3pwTKt34cyn6NrBE7V4eH.jpg"
+        },
+        {
+            title: "Psicopata Americano",
+            poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/d3U7k4hBYgFW7pv4hXUVwkMvum0.jpg"
+        },
+        {
+            title: "Matrix",
+            poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/qFHE09iQDbpHUBXJ8cFZu9v2ViO.jpg"
+        },
+    ],
+
+    fev: [],
+    mar: [],
+    abr: [],
+    mai: [],
+    jun: [],
+    jul: [],
+    ago: [],
+    set: [],
+    out: [],
+    nov: [],
+    dez: []
+};
+
+for (const mes in filmes) {
+    const container = document.getElementById(mes);
+
+    filmes[mes].forEach(filme => {
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('movie');
+        wrapper.dataset.title = filme.title;
+
+        const img = document.createElement('img');
+        img.src = filme.poster;
+        img.loading = 'lazy';
+
+        wrapper.appendChild(img);
+        container.appendChild(wrapper);
+    });
+}
